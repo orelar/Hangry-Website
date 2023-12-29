c246e29d687871e644b23f3bd2910534964df07b
Aurelia Amanda Robert <aurelia.amanda@ui.ac.id>
2023-12-29 12:52:08 +0800

    refactor: Height of page

--- website-slicing/src/components/layout/FindUs.jsx
+++ website-slicing/src/components/layout/FindUs.jsx
@@ -7,7 +7,7 @@ import Eats from "../../assets/eats-logo.png";
 export function FindUs() {
   return (
     <>
-      <div className="bg-white text-center flex flex-col items-center space-y-10 mt-[80rem] py-20 md:py-56 sm:py-52 sm:mt-28 md:mt-32">
+      <div className="bg-white text-center flex flex-col items-center space-y-10 mt-[80rem] py-20 md:py-60 sm:py-52 sm:mt-28 md:mt-32">
         <p className="text-black-hangry md:text-sm text-xs">Hangry! dapat kamu temukan di</p>
         <div className="flex md:space-x-20 sm:space-x-12 space-x-8 mx-2 md:mx-10" id="delivery">
           <img src={Gofood} alt="GoFood Logo" className="md:h-8 sm:h-6 h-3" />
--- website-slicing/src/components/layout/Restaurants.jsx
+++ website-slicing/src/components/layout/Restaurants.jsx
@@ -4,7 +4,7 @@ import { Card } from "../shared/Card";
 export function Restaurants() {
   return (
     <>
-      <div className="md:px-20 bg-white w-full space-y-3 py-10 flex flex-col gap-4 px-8 lg:px-32 bg-red h-[45vh] text-black-hangry">
+      <div className="md:px-20 bg-white w-full space-y-3 py-10 flex flex-col gap-4 px-8 lg:px-32 bg-red h-[70vh] text-black-hangry">
         <h1 className="text-4xl py-2 font-bold ">
           Hangry! adalah restoran dengan beragam brand
         </h1>
